fc680fe19e31a3d6461c3a8a41e264c826acb0a7
Sakuni_Herath_Gallage <saku9706@gmail.com>
2025-02-08 11:29:13 +0530

    initial commit

---
+++ src/main/java/com/example/api_connector_backend/ApiConnectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_connector_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConnectorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnectorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_connector_backend/ApiConnectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_connector_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConnectorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
